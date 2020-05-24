3c4bc6b844a49964954a5a54dd96bbf0218e3278
Christopher Barnes <cbarnes@tmsl.com>
2020-05-24 01:01:49 +0100

    introduced control of speed and angular rotation separately and eliminated Y

--- src/teleop/Teleop.js
+++ src/teleop/Teleop.js
@@ -18,16 +18,17 @@ KEYBOARDTELEOP.Teleop = function(options) {
   var that = this;
   options = options || {};
   var ros = options.ros;
-  var topic = options.topic || '/cmd_vel';
+  var topic = options.topic || 'cmd_vel';
   // permanent throttle
   var throttle = options.throttle || 1.0;
 
   // used to externally throttle the speed (e.g., from a slider)
-  this.scale = 1.0;
-
+  this.scaleR = 1.0;  // Linear
+  this.scaleA = 1.0;  // Angular
+  
   // linear x and y movement and angular z movement
   var x = 0;
-  var y = 0;
+  //var y = 0;
   var z = 0;
 
   var cmdVel = new ROSLIB.Topic({
@@ -40,46 +41,49 @@ KEYBOARDTELEOP.Teleop = function(options) {
   this.handleKey = function(keyCode, keyDown) {
     // used to check for changes in speed
     var oldX = x;
-    var oldY = y;
+    //var oldY = y;
     var oldZ = z;
     
     var pub = true;
 
     var speed = 0;
+    var rotation = 0;
+    
     // throttle the speed by the slider and throttle constant
     if (keyDown === true) {
-      speed = throttle * that.scale;
+      speed = throttle * that.scaleL;
+      rotation = throttle * that.scaleA;  
     }
     // check which key was pressed
     switch (keyCode) {
       case 65:
       case 37:
         // turn left
-        z = 1 * speed;
+        z = rotation;
         break;
       case 87:
       case 38:
         // up
-        x = 0.5 * speed;
+        x = speed;
         break;
       case 68:
       case 39:
         // turn right
-        z = -1 * speed;
+        z = -1 * rotation;
         break;
       case 83:
       case 40:
         // down
-        x = -0.5 * speed;
+        x = -1 * speed;
         break;
-      case 69:
+      //case 69:
         // strafe right
-        y = -0.5 * speed;
-        break;
-      case 81:
+        //y = -0.5 * speed;
+        //break;
+      //case 81:
         // strafe left
-        y = 0.5 * speed;
-        break;
+        //y = 0.5 * speed;
+        //break;
       default:
         pub = false;
     }
@@ -94,14 +98,14 @@ KEYBOARDTELEOP.Teleop = function(options) {
         },
         linear : {
           x : x,
-          y : y,
-          z : z
+          y : 0,
+          z : 0
         }
       });
       cmdVel.publish(twist);
 
       // check for changes
-      if (oldX !== x || oldY !== y || oldZ !== z) {
+      if (oldX !== x || /*oldY !== y*/ || oldZ !== z) {
         that.emit('change', twist);
       }
     }
